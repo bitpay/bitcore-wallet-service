6dea474d19a28774dee6d12d0d4aef0d1d773fb4
Ivan Socolsky <jungans@gmail.com>
2015-03-02 19:24:39 -0300

    recreate

--- test/integration/client.js
+++ test/integration/client.js
@@ -991,6 +991,48 @@ describe('client API ', function() {
           });
         });
       });
+      it('should be able to recreate wallet', function(done) {
+        helpers.createAndJoinWallet(clients, 2, 2, function() {
+          clients[0].createAddress(function(err, addr) {
+            should.not.exist(err);
+            should.exist(addr);
+
+            var db = levelup(memdown, {
+              valueEncoding: 'json'
+            });
+            var storage = new Storage({
+              db: db
+            });
+            var newApp = ExpressApp.start({
+              WalletService: {
+                storage: storage,
+                blockExplorer: blockExplorerMock,
+              },
+              disableLogs: true,
+            });
+
+            var recoveryClient = helpers.newClient(newApp);
+            recoveryClient.import(clients[0].export());
+
+            recoveryClient.getStatus(function(err, status) {
+              should.exist(err);
+              err.code.should.equal('NOTAUTHORIZED');
+              recoveryClient.recreateWallet(function(err) {
+                should.not.exist(err);
+                recoveryClient.getStatus(function(err, status) {
+                  should.not.exist(err);
+                  recoveryClient.createAddress(function(err, addr2) {
+                    should.not.exist(err);
+                    should.exist(addr2);
+                    addr2.should.deep.equal(addr);
+                    done();
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
     });
   });
 
