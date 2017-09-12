56523c7f9e1e1810a6b18040466d3a5b3e22b7de
Raul Nohea Goodness <raulg@hekili.net>
2017-09-12 08:47:55 -1000

    bug fix for "Error: Unknown network" per Issue #1461 https://github.com/bitpay/bitcore/issues/1461

--- bitcorenode/index.js
+++ bitcorenode/index.js
@@ -87,11 +87,11 @@ Service.prototype._getConfiguration = function() {
   // A bitcore-node is either livenet or testnet, so we'll pass
   // the configuration options to communicate via the local running
   // instance of the insight-api service.
-  if (self.node.network === Networks.livenet) {
+  if (self.node.network.name === Networks.livenet.name) {
     baseConfig.blockchainExplorerOpts = {
       livenet: providerOptions
     };
-  } else if (self.node.network === Networks.testnet) {
+  } else if (self.node.network.name === Networks.testnet.name) {
     baseConfig.blockchainExplorerOpts = {
       testnet: providerOptions
     };
