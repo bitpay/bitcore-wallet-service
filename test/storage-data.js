a0fed81b71ceb88891209d1ddf040b38d5c82373
Matias Alejo Garcia <ematiu@gmail.com>
2017-11-09 15:37:37 -0300

    add more tests

---
+++ test/storage-data.js
@@ -0,0 +1,81 @@
+var history = [{
+		"txid" : "1x",
+		"confirmations" : 630,
+		"blockheight" : 428659,
+		"fees" : 13766,
+		"time" : 1473236715,
+		"inputs" : [
+			{
+				"address" : "addr1",
+				"amount" : 22830268
+			}
+		],
+		"outputs" : [
+			{
+				"address" : "addr0",
+				"amount" : 2237164
+			},
+			{
+				"address" : "addr2",
+				"amount" : 20591238
+			}
+    ]
+},
+{
+		"txid" : "2x",
+		"confirmations" : 1000,
+		"blockheight" : 427813,
+		"fees" : 16615,
+		"time" : 1472737486,
+		"inputs" : [
+			{
+				"address" : "addr2",
+				"amount" : 1050000
+			}
+		],
+		"outputs" : [
+			{
+				"address" : "addr3",
+				"amount" : 1535521
+			},
+			{
+				"address" : "addr0",
+				"amount" : 1488655
+			},
+			{
+				"address" : "addr1",
+				"amount" : 3957875
+			},
+			{
+				"address" : "addr2",
+				"amount" : 3856574
+			}
+		]
+},
+{
+		"txid" : "3x",
+		"confirmations" : 994,
+		"blockheight" : 427813,
+		"fees" : 15184,
+		"time" : 1472737486,
+		"inputs" : [
+			{
+				"address" : "addr0",
+				"amount" : 1148865
+			}
+		],
+		"outputs" : [
+			{
+				"address" : "addr1",
+				"amount" : 86442
+			},
+			{
+				"address" : "addr2",
+				"amount" : 1087029
+			}
+		]
+}];
+
+module.exports = {
+  history: history
+};
