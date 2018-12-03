04873f1ecb61cef398319b59f89f65b4d86248c3
Matias Alejo Garcia <ematiu@gmail.com>
2018-12-03 00:21:10 -0300

    travis tests2

--- test/integration/server.js
+++ test/integration/server.js
@@ -359,7 +359,7 @@ describe('Wallet service', function() {
       });
     });
 
-    it.only('should check m-n combination', function(done) {
+    it('should check m-n combination', function(done) {
       var pairs = [{
         m: 0,
         n: 0,
